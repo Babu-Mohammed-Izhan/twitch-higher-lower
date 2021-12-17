525ceff71214439ca5ca1ed8671dac425d900290
Izhan <babuizhan1234@gmail.com>
2021-12-17 12:28:08 +0530

    Added Navber and footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,78 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
-    <div>
-      <div></div>
-    </div>
+    <footer className="text-gray-600 body-font">
+      <div
+        className="
+      container
+      px-5
+      py-24
+      mx-auto
+      flex
+      md:items-center
+      lg:items-start
+      md:flex-row md:flex-nowrap
+      flex-wrap flex-col
+    "
+      >
+        <div className="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left">
+          <Link to="/">
+            <span
+              className=" bg-clip-text text-transparent
+                 bg-gradient-to-r 
+                 from-blue-500 
+                 to-purple-500
+                  text-3xl
+                 cursor-pointer
+                 font-semibold
+                 "
+            >
+              Casity
+            </span>
+          </Link>
+          <p className="mt-2 text-sm text-gray-500">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi,
+            quam?
+          </p>
+        </div>
+        <div
+          className="
+        flex-grow flex flex-wrap
+        md:pl-20
+        -mb-10
+        md:mt-0
+        mt-10
+        md:text-left
+        text-center
+      "
+        ></div>
+      </div>
+      <div className="bg-gray-100">
+        <div
+          className="
+        container
+        mx-auto
+        py-4
+        px-5
+        flex flex-wrap flex-col
+        sm:flex-row
+      "
+        >
+          <p className="text-gray-500 text-sm text-center sm:text-left">
+            Made By
+            <a
+              href="https://babumohammedizhan.netlify.app/"
+              className="text-purple-500 ml-1 font-bold"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Babu Mohammed Izhan
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 };
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,89 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
+  const [hidden, setHidden] = useState(true);
+
+  const handleToggle = () => {
+    setHidden(!hidden);
+  };
+
   return (
-    <div>
-      <div></div>
-    </div>
+    <nav className="bg-white shadow dark:bg-gray-800">
+      <div
+        className="
+      container
+      px-6
+      py-3
+      mx-auto
+      md:flex md:justify-between md:items-center
+      backdrop-blur-lg
+    "
+      >
+        <div className="flex items-center justify-between">
+          <div>
+            <Link
+              className="
+                bg-clip-text text-transparent
+                 bg-gradient-to-r 
+                 from-blue-500 
+                 to-purple-500
+            text-xl
+            font-bold
+            dark:text-white
+            md:text-2xl
+            hover:to-purple-400
+            hover:from-blue-400
+          "
+              to="/"
+            >
+              Casity
+            </Link>
+          </div>
+
+          <div className="flex md:hidden">
+            <button
+              type="button"
+              className="
+            text-gray-500
+            dark:text-gray-200
+            hover:text-gray-600
+            dark:hover:text-gray-400
+            focus:outline-none focus:text-gray-600
+            dark:focus:text-gray-400
+          "
+              aria-label="toggle menu"
+              onClick={() => handleToggle()}
+            >
+              <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current">
+                <path
+                  fillRule="evenodd"
+                  d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+                ></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        <div className={`items-center md:flex ${hidden ? "hidden" : ""}`}>
+          <div className="flex flex-col md:flex-row md:mx-6">
+            <Link
+              className="
+            my-2
+            text-gray-700
+            dark:text-gray-200
+            hover:text-indigo-500
+            dark:hover:text-indigo-400
+            md:mx-4 md:my-0
+          "
+              to="/"
+            >
+              Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </nav>
   );
 };
 
