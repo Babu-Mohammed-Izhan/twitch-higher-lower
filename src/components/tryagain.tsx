355e1d701c2c11e498e630ee2feb13748d7c151d
Izhan <babuizhan1234@gmail.com>
2021-12-20 12:39:09 +0530

    Added bg for everything

--- src/components/tryagain.tsx
+++ src/components/tryagain.tsx
@@ -8,19 +8,22 @@ const Tryagain = ({ points }: TryagainProp) => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex justify-center items-center flex-col  w-full bg-gray-700 opacity-60 h-[93%]">
+    <div className="flex justify-center items-center flex-col  w-full bg-no-repeat bg-cover bg-[url('/src/assests/images/twitch.png')]  h-[92%]">
       <div className="flex items-center justify-center flex-col text-white text-3xl md:text-5xl text-center py-12">
-        Nice! You got {points} point{points === 1 ? null : `s`}. <br />
+        <h1 className="py-5">
+          Nice! You got <span className="font-bold">{points}</span> point
+          {points === 1 ? null : `s`}. <br />
+        </h1>
         Try Again to get a higher score!
       </div>
       <button
-        className=" text-lg mt-5 px-6 py-2 rounded-full hover:outline-offset-8 text-white font-medium outline-1 outline outline-offset-4 outline-white"
+        className=" text-lg mt-5 px-6 py-2 rounded-full hover:outline-offset-8 text-white font-bold outline-1 outline outline-offset-4 outline-white"
         onClick={() => navigate("/game")}
       >
         Try Again
       </button>
       <button
-        className=" text-lg mt-5 px-5 py-2 rounded-full hover:outline-offset-8 text-white font-medium outline-1 outline outline-offset-4 outline-white"
+        className=" text-lg mt-5 px-5 py-2 rounded-full hover:outline-offset-8 text-white font-bold outline-1 outline outline-offset-4 outline-white"
         onClick={() => navigate("/")}
       >
         Homepage
