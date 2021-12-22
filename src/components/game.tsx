dda082a4af1df4cff10c6feabcb721ff5ffdc6f4
Izhan <babuizhan1234@gmail.com>
2021-12-22 20:04:09 +0530

    Added couter animation

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,14 +1,9 @@
 import { useState, Dispatch, SetStateAction, useEffect } from "react";
+import CountUp from "react-countup";
+import { motion } from "framer-motion";
 import { useNavigate } from "react-router-dom";
 import { data } from "../assests/data";
 import TwitchLogo from "../assests/images/android-chrome-512x512.png";
-
-// interface Streamer {
-//   rank: number;
-//   earning: string;
-//   name: string;
-//   login: string;
-// }
 
 interface GameProp {
   score: number;
@@ -30,6 +25,7 @@ const Game = ({ score, setScore }: GameProp) => {
 
   const [correct, setCorrect] = useState(false);
   const [wrong, setWrong] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
   const handleHighorLow = (hl: string) => {
     const firstNumber: number = first.gross_earning;
@@ -41,6 +37,9 @@ const Game = ({ score, setScore }: GameProp) => {
         if (highcheck) {
           setCorrect(true);
           setScore(score + 1);
+          setTimeout(() => {
+            setClicked(true);
+          }, 1500);
         } else {
           setWrong(true);
           setTimeout(() => {
@@ -53,6 +52,9 @@ const Game = ({ score, setScore }: GameProp) => {
         if (lowcheck) {
           setCorrect(true);
           setScore(score + 1);
+          setTimeout(() => {
+            setClicked(true);
+          }, 1500);
         } else {
           setWrong(true);
           setTimeout(() => {
@@ -65,6 +67,7 @@ const Game = ({ score, setScore }: GameProp) => {
     setTimeout(() => {
       setCorrect(false);
       setWrong(false);
+      setClicked(false);
       setFirst(second);
       setSecond(third);
       setThird(data[Math.floor(Math.random() * (data.length - 1))]);
@@ -83,42 +86,79 @@ const Game = ({ score, setScore }: GameProp) => {
   };
 
   return (
-    <div className="grid lg:grid-cols-2 grid-cols-1 w-full bg-purple-900  h-[94%] relative">
-      <div className=" xs:mb-16  flex items-center justify-center flex-col text-white text-2xl md:text-5xl">
+    <div className="w-full bg-purple-900  h-[94%] relative">
+      <div className="grid lg:grid-cols-2 grid-cols-1 h-full">
+        <motion.div
+          animate={clicked ? { y: "-90%" } : { y: 0 }}
+          transition={{ duration: 0.5 }}
+          className=" xs:mb-16  flex items-center justify-center flex-col text-white text-2xl md:text-5xl"
+        >
+          <img
+            className="rounded-full h-14 w-14 text-sm"
+            src={`https://static-cdn.jtvnw.net/jtv_user_pictures/${first.path}`}
+            onError={handleSrcError}
+            alt="profile-pic"
+          />
+          <h1>{first.username}</h1>
+          <h3 className="mt-3">$ {Math.round(first.gross_earning)}</h3>
+        </motion.div>
+        <motion.div
+          animate={clicked ? { y: "-90%" } : { y: 0 }}
+          transition={{ duration: 0.5 }}
+          className="flex items-center justify-center flex-col text-white text-2xl md:text-5xl pt-24 md:pt-[3.6rem]"
+        >
+          <img
+            className="rounded-full h-14 w-14 text-sm"
+            src={`https://static-cdn.jtvnw.net/jtv_user_pictures/${second.path}`}
+            onError={handleSrcError}
+            alt="profile-pic"
+          />
+          <h1>{second.username}</h1>
+          <h3 className="mt-3 h-9">
+            {correct || wrong ? (
+              <>
+                {" "}
+                <CountUp
+                  prefix="USD"
+                  end={Math.round(second.gross_earning)}
+                />{" "}
+              </>
+            ) : (
+              ""
+            )}
+          </h3>
+          <button
+            className={` ${
+              clicked ? "hidden" : ""
+            } text-md md:text-lg mt-5 px-3 py-1 md:px-5 md:py-2 rounded-full  text-white md:font-bold outline-1 outline outline-offset-4 outline-white`}
+            onClick={() => handleHighorLow("high")}
+          >
+            Higher
+          </button>
+          <button
+            className={` ${
+              clicked ? "hidden" : ""
+            } text-md md:text-lg mt-5 px-4 py-1 md:px-5 md:py-2 rounded-full  text-white md:font-bold outline-1 outline outline-offset-4 outline-white`}
+            onClick={() => handleHighorLow("low")}
+          >
+            Lower
+          </button>
+        </motion.div>
+      </div>
+      <motion.div
+        animate={clicked ? { y: "-90%" } : { y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="absolute -top-1/2 xs:mb-16  flex items-center justify-center flex-col text-white text-2xl md:text-5xl"
+      >
         <img
           className="rounded-full h-14 w-14 text-sm"
-          src={`https://static-cdn.jtvnw.net/jtv_user_pictures/${first.path}`}
+          src={`https://static-cdn.jtvnw.net/jtv_user_pictures/${third.path}`}
           onError={handleSrcError}
           alt="profile-pic"
         />
-        <h1>{first.username}</h1>
-        <h3 className="mt-3">$ {Math.round(first.gross_earning)}</h3>
-      </div>
-      <div className="flex items-center justify-center flex-col text-white text-2xl md:text-5xl pt-24 md:pt-[3.6rem]">
-        <img
-          className="rounded-full h-14 w-14 text-sm"
-          src={`https://static-cdn.jtvnw.net/jtv_user_pictures/${second.path}`}
-          onError={handleSrcError}
-          alt="profile-pic"
-        />
-        <h1>{second.username}</h1>
-        <h3 className="mt-3 h-9">
-          {correct || wrong ? `$ ${Math.round(second.gross_earning)}` : ""}
-        </h3>
-
-        <button
-          className="text-md md:text-lg mt-5 px-3 py-1 md:px-5 md:py-2 rounded-full  text-white font-bold outline-1 outline outline-offset-4 outline-white"
-          onClick={() => handleHighorLow("high")}
-        >
-          Higher
-        </button>
-        <button
-          className=" text-md md:text-lg mt-5 px-4 py-1 md:px-5 md:py-2 rounded-full  text-white font-bold outline-1 outline outline-offset-4 outline-white"
-          onClick={() => handleHighorLow("low")}
-        >
-          Lower
-        </button>
-      </div>
+        <h1>{third.username}</h1>
+        <h3 className="mt-3">$ {Math.round(third.gross_earning)}</h3>
+      </motion.div>
       <div
         className={`absolute top-1/2 left-1/2 -translate-y-20 md:-translate-y-30 lg:-translate-y-1/2 -translate-x-1/2 bg-white p-5 px-7 rounded-full text-slate-900 font-bold text-2xl ${
           wrong ? "transition ease-in bg-red-700 duration-400 " : null
